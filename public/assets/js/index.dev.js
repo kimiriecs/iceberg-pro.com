a96c0c7fc2146ce6536eae749a466c87487f0b10
Kimi Riecs <kimiriecs@ex.com>
2021-03-16 00:47:24 +0200

    first commit

---
+++ public/assets/js/index.dev.js
@@ -0,0 +1,9 @@
+//=== Header Slyder
+//=== END Header Slyder
+//=== Fitures Slyder
+//=== END Fitures Slyder
+//=== Servises Slyder
+//=== END Servises Slyder
+//=== Team Slyder
+//=== END Team Slyder
+"use strict";
---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+//=== Header Slyder
+//=== END Header Slyder
+
+
+//=== Fitures Slyder
+//=== END Fitures Slyder
+
+
+//=== Servises Slyder
+//=== END Servises Slyder
+
+
+//=== Team Slyder
+//=== END Team Slyder
